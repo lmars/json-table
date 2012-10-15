a29fa2f9a498ba955bf43d03264ddc472c033c58
Lewis Marshall <lewis.marshall@econsultancy.com>
2012-10-15 11:16:24 +0100

    Add ability to choose some fields

--- app.rb
+++ app.rb
@@ -14,3 +14,28 @@ get '/data-types' do
     { :name => 'Blog Posts' }
   ].to_json
 end
+
+get '/data-fields' do
+  content_type :json
+
+  case params['data_type']
+  when 'Accounts'
+    [
+      { :name => 'First Name' },
+      { :name => 'Last Name' },
+      { :name => 'Email Address' }
+    ].to_json
+  when 'Reports'
+    [
+      { :name => 'Name' },
+      { :name => 'Published Date' },
+      { :name => 'Featured?' }
+    ].to_json
+  when 'Blog Posts'
+    [
+      { :name => 'Title' },
+      { :name => 'Published At' },
+      { :name => 'No. of views' }
+    ].to_json
+  end
+end
--- public/app.js
+++ public/app.js
@@ -13,19 +13,43 @@ DataTypeView = Backbone.View.extend({
     "click": "chooseDataType"
   },
   chooseDataType: function() {
-    console.log(this.model.get('name'));
+    DataFieldCollection.fetch({ data: { data_type: this.model.get('name') } });
+  }
+});
+
+DataFieldCollection = new (Backbone.Collection.extend({
+  url: "/data-fields"
+}));
+
+DataFieldView = Backbone.View.extend({
+  tagName: 'label',
+  template: function(attributes) {
+    return _.template($('#data-field-template').html(), attributes);
+  },
+  render: function() {
+    this.$el.html(this.template(this.model.toJSON()));
+    return this;
   }
 });
 
 App = new (Backbone.View.extend({
   initialize: function() {
     DataTypeCollection.bind('reset', this.renderDataTypes, this);
+    DataFieldCollection.bind('reset', this.renderDataFields, this);
     DataTypeCollection.fetch();
   },
   renderDataTypes: function(data_types) {
     data_types.each(function(data_type) {
       var view = new DataTypeView({ model: data_type });
       $('#data-types').append(view.render().el);
+    });
+  },
+  renderDataFields: function(data_fields) {
+    $('#data-fields').empty();
+
+    data_fields.each(function(data_field) {
+      var view = new DataFieldView({ model: data_field });
+      $('#data-fields').append(view.render().el);
     });
   }
 }));
