695a63758aaf0223a89ffc8d8613d95ff5165f33
Lewis Marshall <lewis.marshall@econsultancy.com>
2012-10-15 12:26:22 +0100

    Add some table headings

--- public/app.js
+++ public/app.js
@@ -13,6 +13,7 @@ DataTypeView = Backbone.View.extend({
     "click": "chooseDataType"
   },
   chooseDataType: function() {
+    $('table thead tr').empty();
     DataFieldCollection.fetch({ data: { data_type: this.model.get('name') } });
   }
 });
@@ -29,6 +30,29 @@ DataFieldView = Backbone.View.extend({
   render: function() {
     this.$el.html(this.template(this.model.toJSON()));
     return this;
+  },
+  events: {
+    "click input": "chooseField"
+  },
+  chooseField: function() {
+    this.model.set('visible', this.$el.find('input').is(':checked'));
+  }
+});
+
+DataColumnView = Backbone.View.extend({
+  tagName: 'th',
+  initialize: function() {
+    this.model.bind('change:visible', this.toggleVisible, this);
+  },
+  render: function() {
+    this.$el.html($('<th>').text(this.model.get('name')));
+    return this;
+  },
+  toggleVisible: function() {
+    if(this.model.get('visible'))
+      $('table thead tr').append(this.render().el);
+    else
+      this.remove();
   }
 });
 
@@ -48,8 +72,10 @@ App = new (Backbone.View.extend({
     $('#data-fields').empty();
 
     data_fields.each(function(data_field) {
-      var view = new DataFieldView({ model: data_field });
-      $('#data-fields').append(view.render().el);
+      var field_view = new DataFieldView({ model: data_field });
+      $('#data-fields').append(field_view.render().el);
+
+      var column_view = new DataColumnView({ model: data_field });
     });
   }
 }));
